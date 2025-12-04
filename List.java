896df7ed8d2e24cf817b24fc9a7d7c5debbee19f
Romanos Iason Potamianos <t8230125@aueb.gr>
2025-12-04 15:09:32 +0200

    List.java file

---
+++ List.java
@@ -0,0 +1,17 @@
+public class List {
+    private int userId;
+    private int listType;
+
+    public int getUserId() {
+        return userId;
+    }
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+    public int getListType() {
+        return listType;
+    }
+    public void setListType(int listType) {
+        this.listType = listType;
+    }
+}
